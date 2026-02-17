5981662ec529a7223e176a1d3e0466b01517f51d
tntfeely-spec <tntfeely@gmail.com>
2026-02-17 11:19:59 -0600

    Update page.tsx
    https://github.com/tntfeely-spec/evermore-directory/edit/main/app/contact/page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -328,7 +328,7 @@ export default function ContactPage() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 Evermore Directory. All rights reserved.</p>
+            <p>&copy; 2026 Evermore Directory. All rights reserved.</p>
           </div>
         </div>
       </footer>
