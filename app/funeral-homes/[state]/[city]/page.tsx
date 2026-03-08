f777e3716e55cd43b09dc14486273fd00e7970e2
tntfeely-spec <tntfeely@gmail.com>
2026-03-08 15:16:08 -0500

    Fix build error and city descriptions query

--- app/funeral-homes/[state]/[city]/page.tsx
+++ app/funeral-homes/[state]/[city]/page.tsx
@@ -339,7 +339,7 @@ backgroundImage: 'url(/Mountain_Lake_Image.webp)',
                     <div className="space-y-2 text-gray-700">
                       <p className="flex items-center">
                         <svg className="w-5 h-5 mr-2 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                          <path strokeLinecap="round" strokeLinejoin=round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
                         {home.address}, {home.city}, {home.state} {home.zip}
